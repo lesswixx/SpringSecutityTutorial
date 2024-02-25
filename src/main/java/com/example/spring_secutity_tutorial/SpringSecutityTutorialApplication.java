8718ba2c7e5b3b690255be1aa43100d7a6b670e9
lesswixx <novikov.egor03@mail.ru>
2024-02-25 15:47:53 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_secutity_tutorial/SpringSecutityTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_secutity_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutityTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecutityTutorialApplication.class, args);
+    }
+
+}
